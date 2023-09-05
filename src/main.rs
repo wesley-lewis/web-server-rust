32c7730941183f98d0c117b57297eea152c52682
wesley-lewis <lewis.wes.23@gmail.com>
2023-09-05 22:56:44 +0530

    added: hello.html, 404.html

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::fs::File;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,10 +17,20 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0;512];
 
     stream.read(&mut buffer).unwrap();
-    
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        println!("Normal");
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else {
+        println!("Error");
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+    let mut contents = String::new();
+    let mut file = File::open(filename).unwrap();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("{}{}", status_line, contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
-
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
