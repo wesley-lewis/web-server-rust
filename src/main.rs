30736147b2c78559c6074b8fdd6bfe9935e95e2e
wesley-lewis <lewis.wes.23@gmail.com>
2023-09-05 22:11:57 +0530

    basic response

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,10 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0;512];
 
     stream.read(&mut buffer).unwrap();
+    
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
